c4754ed841a70c72c75429b0496d576182886d6b
xtuzy <1570571499@qq.com>
2021-01-18 12:19:37 +0800

    Update and rename lib/layouts/standard.js to 自由/布局/standard.js
    注释

--- 自由/布局/standard.js
+++ 自由/布局/standard.js
@@ -1,17 +1,23 @@
 const Layout = require('./layout')
 const Node = require('../hierarchy/node')
 const nonLayeredTidyTree = require('../algorithms/non-layered-tidy-tree')
-
+//继承自基础布局
 class Standard extends Layout {
+//实现基类方法
   doLayout () {
     const me = this
+//布局的根节点?
     const root = me.root
     const options = me.options
     // separate into left and right trees
+//分成左右树
     const leftTree = new Node(root.data, options, true)
     const rightTree = new Node(root.data, options, true)
-    const treeSize = root.children.length
+    //根节点的子节点的数目
+const treeSize = root.children.length
+//右侧树的大小，即子节点数目的一半
     const rightTreeSize = Math.round(treeSize / 2)
+//左右分配节点
     for (let i = 0; i < treeSize; i++) {
       const child = root.children[i]
       if (i < rightTreeSize) {
@@ -21,6 +27,7 @@ class Standard extends Layout {
       }
     }
     // do layout for left and right trees
+//为左右树布局
     nonLayeredTidyTree(rightTree, true)
     nonLayeredTidyTree(leftTree, true)
     leftTree.right2left()
